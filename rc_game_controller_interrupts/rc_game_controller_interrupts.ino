53993e77a350ac707c44b12d218e2d9990f9d3ca
Christian Barran <48329035+justbarran@users.noreply.github.com>
2020-05-24 13:01:14 -0400

    Added Smoothing

--- rc_game_controller_interrupts/rc_game_controller_interrupts.ino
+++ rc_game_controller_interrupts/rc_game_controller_interrupts.ino
@@ -12,11 +12,11 @@
 #include <Joystick.h>
 //#define SERIALPRINT
 
-#define UPDATE_RATE_MS 10
+#define UPDATE_RATE_MS 1
 #define RC_MIN_PULSE_US   1000
 #define RC_MID_PULSE_US   1500
 #define RC_MAX_PULSE_US   2000
-#define PULSE_IN_WAIT_US  25000 //RC Time Between Pulses 20000us 
+#define PULSE_IN_WAIT_US  20000 //RC Time Between Pulses 20000us 
 
 //Pin to Channel
 #define RC_PIN_CH1 7 //THR0
@@ -29,10 +29,10 @@
 #define BUTTON_2 9 //AUX
 
 //Channel to Drone controls
-#define CH_THRO   RC_PIN_CH1
+#define CH_ROLL   RC_PIN_CH1
+#define CH_PITCH  RC_PIN_CH2
+#define CH_THRO   RC_PIN_CH3
 #define CH_YAW    RC_PIN_CH4
-#define CH_PITCH  RC_PIN_CH3
-#define CH_ROLL   RC_PIN_CH2
 #define CH_AUX_1  RC_PIN_CH5
 
 #define JOY_THROTTLE_LOW  1000 
@@ -42,6 +42,7 @@
 #define JOY_RANGE_LOW     1000
 #define JOY_RANGE_HIGH    2000
 
+#define NUM_READINGS 10
 // Create the Joystick
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
   2, 0,                   // Button Count, Hat Switch Count
@@ -50,31 +51,52 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
   true, true,             // rudder,throttle
   false, false, false);   // accelerator, brake, or steering
 
-unsigned long input_thro_rising  = 0;
-unsigned long input_yaw_rising   = 0;
-unsigned long input_pitch_rising = 0;
-unsigned long input_roll_rising  = 0;
-unsigned long input_aux_1_rising = 0;
+volatile unsigned long input_thro_rising  = 0;
+volatile unsigned long input_yaw_rising   = 0;
+volatile unsigned long input_pitch_rising = 0;
+volatile unsigned long input_roll_rising  = 0;
+volatile unsigned long input_aux_1_rising = 0;
 
-unsigned long input_thro_falling  = 0;
-unsigned long input_yaw_falling   = 0;
-unsigned long input_pitch_falling = 0;
-unsigned long input_roll_falling  = 0;
-unsigned long input_aux_1_falling = 0;
+volatile unsigned long input_thro_falling  = 0;
+volatile unsigned long input_yaw_falling   = 0;
+volatile unsigned long input_pitch_falling = 0;
+volatile unsigned long input_roll_falling  = 0;
+volatile unsigned long input_aux_1_falling = 0;
 //RC Controller Values
-unsigned long input_thro  = 0;
-unsigned long input_yaw   = 0;
-unsigned long input_pitch = 0;
-unsigned long input_roll  = 0;
-unsigned long input_aux_1 = 0;
+volatile unsigned long input_thro  = 0;
+volatile unsigned long input_yaw   = 0;
+volatile unsigned long input_pitch = 0;
+volatile unsigned long input_roll  = 0;
+volatile unsigned long input_aux_1 = 0;
+
+int32_t readings_throttle[NUM_READINGS];
+int32_t readings_rudder[NUM_READINGS];
+int32_t readings_x [NUM_READINGS];
+int32_t readings_y[NUM_READINGS];
+int32_t readings_aux_x[NUM_READINGS];
+int32_t readings_aux_y[NUM_READINGS];
+
+int8_t index_throttle = 0;
+int8_t index_rudder = 0;
+int8_t index_x = 0;
+int8_t index_y = 0;
+int8_t index_aux_x = 0;
+int8_t index_aux_y = 0;
+
+int32_t total_throttle = 0;
+int32_t total_rudder = 0;
+int32_t total_x = 0;
+int32_t total_y = 0;
+int32_t total_aux_x = 0;
+int32_t total_aux_y = 0;
 
 //Simulator Values
-int16_t sim_throttle = 0;
-int16_t sim_rudder = 0;
-int16_t sim_x = 0;
-int16_t sim_y = 0;
-int16_t sim_aux_x = 0;
-int16_t sim_aux_y = 0;
+int32_t sim_throttle = 0;
+int32_t sim_rudder = 0;
+int32_t sim_x = 0;
+int32_t sim_y = 0;
+int32_t sim_aux_x = 0;
+int32_t sim_aux_y = 0;
 
 void CH_THRO_ISR(){
   if(digitalRead(CH_THRO) == HIGH){
@@ -92,7 +114,8 @@ void CH_YAW_ISR(){
   if(digitalRead(CH_YAW) == HIGH){
     input_yaw_rising = micros();
   }
-  else{
+  else
+  {
     input_yaw_falling = micros();
     if(input_yaw_falling>input_yaw_rising){ //Check for overflow
     input_yaw = input_yaw_falling-input_yaw_rising;
@@ -103,7 +126,8 @@ void CH_PITCH_ISR(){
   if(digitalRead(CH_PITCH) == HIGH){
     input_pitch_rising = micros();
   }
-  else{
+  else
+  {
     input_pitch_falling = micros();
     if(input_pitch_falling>input_pitch_rising){//Check for overflow
     input_pitch = input_pitch_falling-input_pitch_rising;
@@ -114,7 +138,8 @@ void CH_ROLL_ISR(){
   if(digitalRead(CH_ROLL) == HIGH){
     input_roll_rising = micros();
   }
-  else{
+  else
+  {
     input_roll_falling = micros();
     if(input_roll_falling>input_roll_rising){//Check for overflow
     input_roll = input_roll_falling-input_roll_rising;
@@ -125,7 +150,8 @@ void CH_AUX_1_ISR(){
   if(digitalRead(CH_AUX_1) == HIGH){
     input_aux_1_rising = micros();
   }
-  else{
+  else
+  {
     input_aux_1_falling = micros();
     if(input_aux_1_falling>input_aux_1_rising){//Check for overflow
     input_aux_1 = input_aux_1_falling-input_aux_1_rising;
@@ -141,11 +167,15 @@ void setup() {
   pinMode(RC_PIN_CH5, INPUT);
   pinMode(BUTTON_1,   INPUT);
   pinMode(BUTTON_2,   INPUT);
-  attachInterrupt(digitalPinToInterrupt(CH_THRO),   CH_THRO_ISR,  CHANGE);
-  attachInterrupt(digitalPinToInterrupt(CH_YAW),    CH_YAW_ISR,   CHANGE);
-  attachInterrupt(digitalPinToInterrupt(CH_PITCH),  CH_PITCH_ISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(CH_ROLL),   CH_ROLL_ISR,  CHANGE);
-  attachInterrupt(digitalPinToInterrupt(CH_AUX_1),  CH_AUX_1_ISR, CHANGE);
+  
+  for (int thisReading = 0; thisReading < NUM_READINGS; thisReading++) {
+      readings_throttle[thisReading]=0;
+      readings_rudder[thisReading]=0;
+      readings_x [thisReading]=0;
+      readings_y[thisReading]=0;
+      readings_aux_x[thisReading]=0;
+      readings_aux_y[thisReading]=0;
+  }
 #ifdef SERIALPRINT
   Serial.begin(115200);
 #endif
@@ -158,31 +188,70 @@ void setup() {
   Joystick.setYAxisRange(JOY_RANGE_LOW,JOY_RANGE_HIGH);
   Joystick.setButton(0,0);
   Joystick.setButton(1,0); 
+  
+  attachInterrupt(digitalPinToInterrupt(CH_THRO),   CH_THRO_ISR,  CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH_YAW),    CH_YAW_ISR,   CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH_PITCH),  CH_PITCH_ISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH_ROLL),   CH_ROLL_ISR,  CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH_AUX_1),  CH_AUX_1_ISR, CHANGE);
+  delay(1);
 }
 
 void loop() {  
-  sim_throttle =  map(input_thro,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_THROTTLE_LOW,JOY_THROTTLE_HIGH);
-  Joystick.setThrottle(sim_throttle);
+  total_throttle = total_throttle - readings_throttle[index_throttle];
+  readings_throttle[index_throttle] =  map(input_thro,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_THROTTLE_LOW,JOY_THROTTLE_HIGH);
+  total_throttle = total_throttle + readings_throttle[index_throttle];
+  index_throttle++;
+  if(index_throttle>= NUM_READINGS)
+  {
+    index_throttle = 0;
+  }
+  sim_throttle = total_throttle/NUM_READINGS;
+  Joystick.setThrottle(sim_throttle);  
 #ifdef SERIALPRINT
   Serial.print(sim_throttle);
   Serial.print("\t");    
 #endif
-  sim_rudder =    map(input_yaw,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_RUDDER_LOW,JOY_RUDDER_HIGH);
+  total_rudder = total_rudder - readings_rudder[index_rudder];
+  readings_rudder[index_rudder] = map(input_yaw,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_RUDDER_LOW,JOY_RUDDER_HIGH);
+  total_rudder = total_rudder + readings_rudder[index_rudder];
+  index_rudder++;
+  if(index_rudder>= NUM_READINGS)
+  {
+    index_rudder = 0;
+  }
+  sim_rudder = total_rudder/NUM_READINGS;
   Joystick.setRudder(sim_rudder);
 #ifdef SERIALPRINT
   Serial.print(sim_rudder);
   Serial.print("\t");
 #endif
-  sim_y =         map(input_pitch,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_RANGE_LOW,JOY_RANGE_HIGH);
+  total_y = total_y - readings_y[index_y];
+  readings_y[index_y]= map(input_pitch,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_RANGE_LOW,JOY_RANGE_HIGH);
+   total_y = total_y + readings_y[index_y];
+  index_y++;
+  if(index_y>= NUM_READINGS)
+  {
+    index_y = 0;
+  }
+  sim_y = total_y/NUM_READINGS;
   Joystick.setYAxis(sim_y);
 #ifdef SERIALPRINT
-  Serial.print(sim_x);
+  Serial.print(sim_y);
   Serial.print("\t");
 #endif
-  sim_x =         map(input_roll,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_RANGE_LOW,JOY_RANGE_HIGH);
+  total_x = total_x - readings_x[index_x];
+  readings_x[index_x]=map(input_roll,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_RANGE_LOW,JOY_RANGE_HIGH);
+  total_x = total_x + readings_x[index_x];
+  index_x++;
+  if(index_x>= NUM_READINGS)
+  {
+    index_x = 0;
+  }
+  sim_x = total_x/NUM_READINGS;
   Joystick.setXAxis(sim_x);
 #ifdef SERIALPRINT
-  Serial.print(sim_y);
+  Serial.print(sim_x);
   Serial.print("\t"); 
 #endif
   sim_aux_x =     map(input_aux_1,RC_MIN_PULSE_US,RC_MAX_PULSE_US,JOY_RANGE_LOW,JOY_RANGE_HIGH);
